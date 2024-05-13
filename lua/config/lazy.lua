77f835576e37cfe64dcaf832e4c61ce14181fa78
mattcompiles <mattjones701@gmail.com>
2024-05-13 15:40:13 +1000

    Close neotree on select, add JFE find command

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -49,28 +49,4 @@ require("lazy").setup({
   },
 })
 
--- local function is_flowtype()
---   ---@diagnostic disable-next-line: undefined-field
---   return vim.fn.getline(1):match("//%s*@flow")
--- end
---
--- vim.filetype.add({
---   extension = {
---     flow = "flowtype",
---     js = function()
---       if is_flowtype() then
---         return "flowtype"
---       end
---       return "javascript"
---     end,
---
---     jsx = function()
---       if is_flowtype() then
---         return "flowtypereact"
---       end
---       return "javascriptreact"
---     end,
---   },
--- })
-
 require("lspconfig").flow.setup({})
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,1 +1,40 @@
-return {}
+return {
+  "jeffkreeftmeijer/vim-numbertoggle",
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- disable the keymap to grep files
+      -- { "<leader>/", false },
+      -- change a keymap
+      {
+        "<leader>fp",
+        function()
+          require("telescope.builtin").find_files({ search_dirs = { "build-tools", "dev-tooling" } })
+        end,
+        desc = "Find Jira tools",
+      },
+      -- add a keymap to browse plugin files
+      -- {
+      --   "<leader>fp",
+      --   function()
+      --     require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+      --   end,
+      --   desc = "Find Plugin File",
+      -- },
+    },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      close_if_last_window = true,
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function()
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
+    },
+  },
+}
