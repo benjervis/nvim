ddfc2bdaa80e62e58c812c5200841e8bec4c8509
Ben Jervis <bjervis@atlassian.com>
2024-05-23 15:57:24 +1000

    Delete some commented out boilerplate config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -77,14 +77,6 @@ return {
         end,
         desc = "Find packages",
       },
-      -- add a keymap to browse plugin files
-      -- {
-      --   "<leader>fp",
-      --   function()
-      --     require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
-      --   end,
-      --   desc = "Find Plugin File",
-      -- },
     },
   },
   {
